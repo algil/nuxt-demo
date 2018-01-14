31790d4a000b8ef2c44aa800f069a323a1f16b11
Antonio Gil <antonio.luis.gil@gmail.com>
2018-01-14 14:15:22 +0100

    Fist commit

---
+++ pages/user/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h2>User: {{ user.name }}</h2>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  transition: 'user',
+
+  async asyncData ({params}) {
+    let {data} = await axios.get(`https://api.github.com/users/${params.id}`)
+    return { user: data }
+  }
+}
+</script>
